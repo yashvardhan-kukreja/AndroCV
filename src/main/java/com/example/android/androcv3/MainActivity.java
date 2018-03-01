b59e8120f79f00e98aee52f877520ea23d9c500b
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-03-02 02:02:30 +0530

    Added Canny edge detection with good accuracy and less noise

--- src/main/java/com/example/android/androcv3/MainActivity.java
+++ src/main/java/com/example/android/androcv3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.androcv3;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -37,8 +38,8 @@ import java.io.InputStream;
 public class MainActivity extends AppCompatActivity{
 
     JavaCameraView javaCameraView;
-    Mat mRgba, mRgbaT, mGray, mThreshold;
-    Mat mRgbaFD, mRgbaTFD, mGrayFD, mThresholdFD;
+    Mat mRgba, mRgbaT, mGray, mThreshold, mInvert, mEdges;
+    Mat mRgbaTFD, mGrayFD, mThresholdFD;
     Toolbar toolbar;
     Button toggleCam, toggleMode;
     Switch toggleFaceDetection;
@@ -99,12 +100,15 @@ public class MainActivity extends AppCompatActivity{
         @Override
         public void onCameraViewStarted(int width, int height) {
             mRgba = new Mat(height, width, CvType.CV_16UC4);
+            mInvert = new Mat(height, width, CvType.CV_16UC4);
             mRgbaT = new Mat(height, width, CvType.CV_16UC4);
             mGray = new Mat(height, width, CvType.CV_16UC4);
             mThreshold = new Mat(height, width, CvType.CV_16UC4);
             mRgbaTFD = new Mat(height, width, CvType.CV_16UC4);
             mGrayFD = new Mat(height, width, CvType.CV_16UC4);
             mThresholdFD = new Mat(height, width, CvType.CV_16UC4);
+            mEdges = new Mat(height, width, CvType.CV_16UC4);
+
             absFaceSize = (int)(height*0.25);
         }
 
@@ -114,8 +118,12 @@ public class MainActivity extends AppCompatActivity{
                 mRgba.release();
             else if (mode == 1)
                 mGray.release();
-            else
+            else if (mode == 2)
                 mThreshold.release();
+            else if (mode == 3)
+                mEdges.release();
+            else
+                mInvert.release();
         }
 
         @Override
@@ -132,11 +140,17 @@ public class MainActivity extends AppCompatActivity{
             if (index == 1)
                 Core.flip(mRgbaT, mRgbaT, Core.BORDER_CONSTANT);
 
+            // Applying bitwise not to generate an inverted frame
+            Core.bitwise_not(mRgbaT, mInvert);
+
             // Resizing mRgbaT to the size of original input frame
             Imgproc.resize(mRgbaT, mRgbaT, mRgba.size());
 
             // Extracting a grayscale image of the mRgbaT and then, storing it in mGray
             Imgproc.cvtColor(mRgbaT, mGray, Imgproc.COLOR_RGB2GRAY);
+
+            // Extracting Canny edges and storing them in mEdges Mat
+            Imgproc.Canny(mGray, mEdges, 100, 255);
 
             // Extracting a threshold image of mGray and then, storing it in mThreshold
             Core.inRange(mGray, new Scalar(45, 20, 10), new Scalar(75, 255, 255), mThreshold);
@@ -148,13 +162,13 @@ public class MainActivity extends AppCompatActivity{
             MatOfRect faces = new MatOfRect();
 
 
-            if (faceClassifier != null){
+            if (faceClassifier != null) {
                 faceClassifier.detectMultiScale(mGray, faces,  1.1, 2, 2, new Size(absFaceSize, absFaceSize), new Size());
                 Rect[] facesArray = faces.toArray();
                 for (Rect rect : facesArray){
-                    Imgproc.rectangle(mRgbaT, rect.tl(), rect.br(), new Scalar(255, 0, 0), 3);
-                    Imgproc.rectangle(mGray, rect.tl(), rect.br(), new Scalar(255, 255, 255), 3);
-                    Imgproc.rectangle(mThreshold, rect.tl(), rect.br(), new Scalar(0, 255, 255), 2);
+                    Imgproc.rectangle(mRgbaTFD, rect.tl(), rect.br(), new Scalar(255, 0, 0), 3);
+                    Imgproc.rectangle(mGrayFD, rect.tl(), rect.br(), new Scalar(255, 255, 255), 3);
+                    Imgproc.rectangle(mThresholdFD, rect.tl(), rect.br(), new Scalar(0, 255, 255), 2);
                 }
             }
 
@@ -164,16 +178,24 @@ public class MainActivity extends AppCompatActivity{
                     return mRgbaTFD;
                 else if (mode == 1)
                     return mGrayFD;
-                else
+                else if (mode == 2)
                     return mThresholdFD;
+                else if (mode == 3)
+                    return mEdges;
+                else
+                    return mInvert;
             } else {
                 // Returning the Mat on the basis of the toggle mode provided by the user
                 if (mode == 0)
                     return mRgbaT;
                 else if (mode == 1)
                     return mGray;
-                else
+                else if (mode == 2)
                     return mThreshold;
+                else if (mode == 3)
+                    return mEdges;
+                else
+                    return mInvert;
             }
 
         }
@@ -190,10 +212,9 @@ public class MainActivity extends AppCompatActivity{
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
         }
 
-        if (OpenCVLoader.initDebug()){
+        if (!OpenCVLoader.initDebug()){
             Toast.makeText(MainActivity.this, "Sorted !!!", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(MainActivity.this, "Unsorted !!!", Toast.LENGTH_LONG).show();
+            return;
         }
 
         javaCameraView = findViewById(R.id.javaCamView);
@@ -240,6 +261,10 @@ public class MainActivity extends AppCompatActivity{
                     mode = 1;
                 else if (mode == 1)
                     mode = 2;
+                else if (mode == 2)
+                    mode = 3;
+                else if (mode == 3)
+                    mode = 4;
                 else
                     mode = 0;
                 javaCameraView.setCvCameraViewListener(cvCameraViewListener2);
